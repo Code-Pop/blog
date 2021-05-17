3d240d3f599ac697f13c75a083826924e24e77d5
Andy <andy@Andys-MacBook-Pro.local>
2021-05-17 16:42:40 -0400

    vue 2 sample app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Heading msg="Compat App" />
+    <ul>
+      <li v-for="item in items" v-bind:key="item">{{ item }}</li>
+    </ul>
+      <button v-on:click="pop">Pop</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Heading from './components/Heading.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Heading
+  },
+  data() {
+    return {
+      items: [1,2,3,4,5]
+    }
+  },
+  watch: {
+    items: {
+      handler(val, oldVal) {
+        console.log(oldVal + ' --> ' + val)
+      }
+    }
+  },
+  methods: {
+    pop() {
+      this.items.pop()
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+  background-color:#eee;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+ul {
+  padding-left: 0;
+  list-style: none;
+}
+
+button {
+  background-color: red;
+  padding: 5px 15px;
+  border: none;
+  color:white;
+  font-size: 16pt;
 }
 </style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,12 @@
+<template functional>
+  <h1>{{ props.msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Heading',
+  props: {
+    msg: String
+  }
+}
+</script>
